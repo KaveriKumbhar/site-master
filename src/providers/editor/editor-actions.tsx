6edb0f9bd552e359714f9f872b96133ee2a5b40b
ABSOLUTE-NOTHING <116956963+ABSOLUTE-NOTHING@users.noreply.github.com>
2024-08-18 23:14:53 +0530

    added a global state manager store

--- src/providers/editor/editor-actions.tsx
+++ src/providers/editor/editor-actions.tsx
@@ -1,66 +1,72 @@
-import { DeviceTypes, EditorElement } from './editor-provider'
+import { DeviceTypes, EditorElement } from "./editor-provider";
 
 export type EditorAction =
   | {
-      type: 'ADD_ELEMENT'
+      type: "ADD_ELEMENT";
       payload: {
-        containerId: string
-        elementDetails: EditorElement
-      }
+        containerId: string;
+        elementDetails: EditorElement;
+      };
     }
   | {
-      type: 'UPDATE_ELEMENT'
+      type: "UPDATE_ELEMENT";
       payload: {
-        elementDetails: EditorElement
-      }
+        elementDetails: EditorElement;
+      };
     }
   | {
-      type: 'DELETE_ELEMENT'
+      type: "DELETE_ELEMENT";
       payload: {
-        elementDetails: EditorElement
-      }
+        elementDetails: EditorElement;
+      };
     }
   | {
-      type: 'CHANGE_CLICKED_ELEMENT'
+      type: "CHANGE_CLICKED_ELEMENT";
       payload: {
         elementDetails?:
           | EditorElement
           | {
-              id: ''
-              content: []
-              name: ''
-              styles: {}
-              type: null
-            }
-      }
+              id: "";
+              content: [];
+              name: "";
+              styles: {};
+              type: null;
+            };
+      };
     }
   | {
-      type: 'CHANGE_DEVICE'
+      type: "CHANGE_DEVICE";
       payload: {
-        device: DeviceTypes
-      }
+        device: DeviceTypes;
+      };
     }
   | {
-      type: 'TOGGLE_PREVIEW_MODE'
+      type: "TOGGLE_PREVIEW_MODE";
     }
   | {
-      type: 'TOGGLE_LIVE_MODE'
+      type: "TOGGLE_LIVE_MODE";
       payload?: {
-        value: boolean
-      }
+        value: boolean;
+      };
     }
-  | { type: 'REDO' }
-  | { type: 'UNDO' }
+  | { type: "REDO" }
+  | { type: "UNDO" }
   | {
-      type: 'LOAD_DATA'
+      type: "LOAD_LOCALSTORAGE";
       payload: {
-        elements: EditorElement[]
-        withLive: boolean
-      }
+        funnelPageId: string;
+      };
     }
   | {
-      type: 'SET_FUNNELPAGE_ID'
+      type: "LOAD_DATA";
       payload: {
-        funnelPageId: string
-      }
+        elements: EditorElement[];
+        withLive: boolean;
+      };
     }
+  | {
+      type: "SET_FUNNELPAGE_ID";
+      payload: {
+        funnelPageId: string;
+      };
+    };
--- src/providers/editor/editor-provider.tsx
+++ src/providers/editor/editor-provider.tsx
@@ -2,6 +2,7 @@
 
 import { EditorBtns } from "@/lib/constants";
 import { EditorAction } from "./editor-actions";
+import { createContext, Dispatch, useContext, useReducer } from "react";
 
 // ----------------------------- TYPES -----------------------------------
 
@@ -21,7 +22,7 @@ export type Editor = {
   selectedElement: EditorElement;
   device: DeviceTypes;
   previewMode: boolean;
-  // funnelPageId: string
+  funnelPageId: string;
 };
 
 export type HistoryState = {
@@ -58,7 +59,7 @@ const initialEditorState: EditorState["editor"] = {
   device: "Desktop",
   previewMode: false,
   live: false,
-  // funnelPageId: '',
+  funnelPageId: "",
 };
 
 // History array's default values
@@ -102,6 +103,52 @@ const addElement = (
   });
 };
 
+const updateAnElement = (
+  editorArray: EditorElement[],
+  action: EditorAction
+): EditorElement[] => {
+  if (action.type !== "UPDATE_ELEMENT") {
+    throw Error("You sent the wrong type to the update element editor state");
+  }
+
+  return editorArray.map((item) => {
+    if (item.id === action.payload.elementDetails.id) {
+      return {
+        ...item,
+        ...action.payload.elementDetails,
+      };
+    } else if (item.content && Array.isArray(item.content)) {
+      return {
+        ...item,
+        content: updateAnElement(item.content, action),
+      };
+    }
+
+    return item;
+  });
+};
+
+const deleteAnElement = (
+  editorArray: EditorElement[],
+  action: EditorAction
+): EditorElement[] => {
+  if (action.type !== "DELETE_ELEMENT") {
+    throw new Error(
+      "You sent the wrong type to the delete element editor state"
+    );
+  }
+
+  return editorArray.filter((item) => {
+    if (item.id === action.payload.elementDetails.id) {
+      return false;
+    } else if (item.content && Array.isArray(item.content)) {
+      item.content = deleteAnElement(item.content, action);
+    }
+
+    return true;
+  });
+};
+
 // Function to handle each type action we want to be performed on EDITOR
 
 const editorReducer = (
@@ -110,17 +157,265 @@ const editorReducer = (
 ): EditorState => {
   switch (action.type) {
     case "ADD_ELEMENT":
+      const updatedEditorState = {
+        ...state.editor,
+        elements: addElement(state.editor.elements, action),
+      };
+
+      const updatedHistory = [
+        ...state.history.history,
+        // ...state.history.history.slice(0, state.history.currentIdx + 1),
+        { ...updatedEditorState },
+      ];
+
+      const newEditorState: EditorState = {
+        ...state,
+        editor: updatedEditorState,
+        history: {
+          ...state.history,
+          history: updatedHistory,
+          currentIdx: updatedHistory.length - 1,
+        },
+      };
+
+      return newEditorState;
 
     case "UPDATE_ELEMENT":
+      const updatedElements = updateAnElement(state.editor.elements, action);
+
+      const UpdatedElementIsSelected =
+        state.editor.selectedElement.id === action.payload.elementDetails.id;
+
+      const updatedEditorStateAfterUpdate: Editor = {
+        ...state.editor,
+        elements: updatedElements,
+        selectedElement: UpdatedElementIsSelected
+          ? action.payload.elementDetails
+          : {
+              id: "",
+              content: [],
+              name: "",
+              styles: {},
+              type: null,
+            },
+      };
+
+      const updatedHistoryAfterUpdate = [
+        ...state.history.history,
+        { ...updatedEditorStateAfterUpdate },
+      ];
+
+      const updatedEditor: EditorState = {
+        ...state,
+        editor: updatedEditorStateAfterUpdate,
+        history: {
+          ...state.history,
+          history: updatedHistoryAfterUpdate,
+          currentIdx: updatedHistoryAfterUpdate.length - 1,
+        },
+      };
+
+      return updatedEditor;
+
     case "DELETE_ELEMENT":
+      const updatedElementsAfterDelete = deleteAnElement(
+        state.editor.elements,
+        action
+      );
+
+      const updatedEditorStateAfterDelete: Editor = {
+        ...state.editor,
+        elements: updatedElementsAfterDelete,
+      };
+
+      const updatedHistoryStateAfterDelete = [
+        ...state.history.history,
+        { ...updatedEditorStateAfterDelete },
+      ];
+
+      const deletedState = {
+        ...state,
+        editor: updatedEditorStateAfterDelete,
+        history: {
+          history: updatedHistoryStateAfterDelete,
+          currentIdx: updatedHistoryStateAfterDelete.length - 1,
+        },
+      };
+
+      return deletedState;
+
     case "CHANGE_CLICKED_ELEMENT":
+      const newSelectedElement =
+        action.payload.elementDetails || {
+          id: "",
+          content: [],
+          name: "",
+          styles: {},
+          type: null,
+        } ||
+        initialState;
+
+      // Can be Optimized - can be removed when better memory utilization is needed;
+
+      const clickedState = {
+        ...state,
+        editor: {
+          ...state.editor,
+          selectedElement: action.payload.elementDetails || {
+            id: "",
+            content: [],
+            name: "",
+            styles: {},
+            type: null,
+          },
+          // || initialState
+        },
+        history: {
+          ...state.history,
+          history: [
+            ...state.history.history.slice(0, state.history.currentIdx + 1),
+            {
+              ...state.editor,
+              selectedElement: newSelectedElement,
+            },
+          ],
+          currentIdx: state.history.currentIdx + 1,
+        },
+      };
+
+      // In case above code is not supported , try running this one :s
+
+      // const clickedState = {
+      //   ...state,
+      //   editor: {
+      //     ...state.editor,
+      //     selectedElement: action.payload.elementDetails || {
+      //       id: "",
+      //       content: [],
+      //       name: "",
+      //       styles: {},
+      //       type: null,
+      //     },
+      //     // || initialState
+      //   },
+      //   history: {
+      //     ...state.history,
+      //     history: [
+      //       ...state.history.history.slice(0, state.history.currentIdx + 1),
+      //       { ...state.editor },
+      //     ],
+      //     currentIdx: state.history.currentIdx + 1,
+      //   },
+      // };
+
+      return clickedState;
+
     case "CHANGE_DEVICE":
+      const changedDeviceState: EditorState = {
+        ...state,
+        editor: {
+          ...state.editor,
+          device: action.payload.device,
+        },
+      };
+
+      return changedDeviceState;
+
     case "TOGGLE_PREVIEW_MODE":
+      const toggleState: EditorState = {
+        ...state,
+        editor: {
+          ...state.editor,
+          previewMode: !state.editor.previewMode,
+        },
+      };
+
+      return toggleState;
+
     case "TOGGLE_LIVE_MODE":
+      const toggleLiveMode: EditorState = {
+        ...state,
+        editor: {
+          ...state.editor,
+          live: action.payload ? action.payload.value : !state.editor.live,
+        },
+      };
+
+      return toggleLiveMode;
+
     case "REDO":
+      if (state.history.currentIdx < state.history.history.length - 1) {
+        const nextIndex = state.history.currentIdx + 1;
+        const nextEditorState = {
+          ...state.history.history[nextIndex],
+        };
+        const redoState: EditorState = {
+          ...state,
+          editor: nextEditorState,
+          history: {
+            ...state.history,
+            currentIdx: nextIndex,
+          },
+        };
+
+        return redoState;
+      }
+
+      return state;
+
     case "UNDO":
+      if (state.history.currentIdx > 0) {
+        const prevIndex = state.history.currentIdx - 1;
+        const prevEditorState = {
+          ...state.history.history[prevIndex],
+        };
+        const undoState: EditorState = {
+          ...state,
+          editor: prevEditorState,
+          history: {
+            ...state.history,
+            currentIdx: prevIndex,
+          },
+        };
+
+        return undoState;
+      }
+      return state;
+
+    // case "LOAD_LOCALSTORAGE":
+    //   const dataFromStorage = localStorage.getItem(action.payload.funnelPageId);
+    //   if (dataFromStorage) return JSON.parse(dataFromStorage);
+    //   else return state;
+
     case "LOAD_DATA":
+      return {
+        ...initialState,
+        editor: {
+          ...initialState.editor,
+          elements: action.payload.elements || initialEditorState.elements,
+          live: !!action.payload.withLive,
+        },
+      };
     case "SET_FUNNELPAGE_ID":
+      const { funnelPageId } = action.payload;
+      const updatedEditorStateWithFunnelPageId = {
+        ...state.editor,
+        funnelPageId,
+      };
+      const updatedHistoryStateWithFunnelPageId = [
+        ...state.history.history.slice(0, state.history.currentIdx + 1),
+        { ...updatedEditorStateWithFunnelPageId },
+      ];
+
+      const funnelPageIdState: EditorState = {
+        ...state,
+        editor: updatedEditorStateWithFunnelPageId,
+        history: {
+          ...state.history,
+          history: updatedHistoryStateWithFunnelPageId,
+          currentIdx: updatedHistoryStateWithFunnelPageId.length - 1,
+        },
+      };
     default:
       return state;
   }
@@ -131,6 +426,62 @@ const editorReducer = (
 // create Editor Context (createContext({  state: EditorState,
 //                        dispatch: Dispatch<EditorAction> }))
 
+export type EditorContextData = {
+  device: DeviceTypes;
+  previewMode: boolean;
+  setPreviewMode: (previewMode: boolean) => void;
+  setDevice: (device: DeviceTypes) => void;
+};
+
 // create Editor provider component
 
+export const EditorContext = createContext<{
+  state: EditorState;
+  dispatch: Dispatch<EditorAction>;
+  subaccountId: string;
+  funnelId: string;
+  // pageDetails: FunnelPage | null  // Prisma Client
+}>({
+  state: initialState,
+  dispatch: () => undefined,
+  subaccountId: "",
+  funnelId: "",
+  // pageDetails: null,
+});
+
 // create a custom hook named as "useEditor" to use the context
+
+type EditorProps = {
+  children: React.ReactNode;
+  subaccountId: string;
+  funnelId: string;
+  // pageDetails: FunnelPage
+};
+
+const EditorProvider = (props: EditorProps) => {
+  const [state, dispatch] = useReducer(editorReducer, initialState);
+
+  return (
+    <EditorContext.Provider
+      value={{
+        state,
+        dispatch,
+        subaccountId: props.subaccountId,
+        funnelId: props.funnelId,
+        // pageDetails: props.pageDetails,
+      }}
+    >
+      {props.children}
+    </EditorContext.Provider>
+  );
+};
+
+export const useEditor = () => {
+  const context = useContext(EditorContext);
+  if (!context) {
+    throw new Error("useEditor Hook must be used within the editor Provider");
+  }
+  return context;
+};
+
+export default EditorProvider;
