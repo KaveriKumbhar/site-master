efe236dc324205c5b4f13c8e069b7eec7a1b2a78
ABSOLUTE-NOTHING <116956963+ABSOLUTE-NOTHING@users.noreply.github.com>
2024-09-02 21:50:45 +0530

    added clerk auth | updated middleware handling for clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <TooltipProvider>
-            <ThemeProvider
+            <ThemeProvider 
               attribute="class"
               defaultTheme="system"
               enableSystem
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,12 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   '/sign-in',
   '/sign-up',
+  '/',
+  '/site',
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  // if (!isPublicRoute(req)) auth().protect();
+  if (!isPublicRoute(req)) auth().protect();
 });
 
 export const config = {
